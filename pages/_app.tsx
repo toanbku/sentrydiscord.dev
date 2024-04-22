a82492947ed619443ced3fd3d6c6818538372b07
Toan Ho <toanbku@gmail.com>
2024-04-22 20:38:38 +0700

    chore: update base url

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,7 +18,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://sentrydiscord.dev/" />
+        <meta
+          property="og:url"
+          content={`https://${process.env.NEXT_PUBLIC_BASE_URL}/`}
+        />
         <meta property="og:title" content="Sentry → Discord" />
         <meta
           property="og:description"
@@ -26,11 +29,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta
           property="og:image"
-          content="https://sentrydiscord.dev/share.png"
+          content={`https://${process.env.NEXT_PUBLIC_BASE_URL}/share.png`}
         />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="https://sentrydiscord.dev/" />
+        <meta
+          property="twitter:url"
+          content={`https://${process.env.NEXT_PUBLIC_BASE_URL}/`}
+        />
         <meta property="twitter:title" content="Sentry → Discord" />
         <meta
           property="twitter:description"
@@ -38,7 +44,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         />
         <meta
           property="twitter:image"
-          content="https://sentrydiscord.dev/share.png"
+          content={`https://${process.env.NEXT_PUBLIC_BASE_URL}/share.png`}
         />
 
         <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
